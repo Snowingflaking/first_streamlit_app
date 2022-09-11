ebc2c2a382da6293b9658822a7f570a8f08263df
learning <113296691+Snowingflaking@users.noreply.github.com>
2022-09-11 10:22:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
