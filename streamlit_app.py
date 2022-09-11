56bf78a90007d3b742359ade1a976d62d0ddc5ef
learning <113296691+Snowingflaking@users.noreply.github.com>
2022-09-11 09:58:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Diner')
-'idly sambar'
-'dosa'
-streamlit.header('Breakfast Menu')
-streamlit.text('Cereal Milk, and Banana')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
