ac4bb26842ec95da4286135f2215975042bccf42
learning <113296691+Snowingflaking@users.noreply.github.com>
2022-09-14 10:49:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.write('The user entered ', fruit_choice)
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  # line 33
 
 # streamlit.text(fruityvice_response)
 
