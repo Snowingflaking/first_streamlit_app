49bcee0330ff44ac58d059a1dc6e137d52b55d1d
learning <113296691+Snowingflaking@users.noreply.github.com>
2022-09-14 10:58:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,19 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response - line 24 in lessons
 # variables ---  after all below are done!
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = requests.get('https://fruityvice.com/api/fruit/')  # line 33
+  if not fruit_choice:
+    streamlit.error ("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
+    
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
